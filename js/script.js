64d1dfa4bd3b5fd1ba13ba6bde55ead64c702457
Matt Coston <mattcoston7@gmail.com>
2017-11-07 20:22:55 -0600

    Added basic files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+//document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+randomNumber = Math.floor( Math.random() * 6) + 1
+console.log('hey' + randomNumber);
+
+var quotes = {
+	quote: "To be or not to be"
+	source: "William Shakespeare"
+	citation: "Hamlet"
+	year: "Middle English"
+}
+
+// rgbColor = Math.floor(Math.random() * 255) + 1
+// alert(rgbColor);
+// onLoad.setBackgroundColor(rgbColor)
+
+// rgbColorFunction ()
